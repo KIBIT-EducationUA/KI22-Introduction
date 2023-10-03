900e67aecdbb0ce351251ade8dbdd54c2c8db78f
Your Name <you@example.com>
2023-10-03 23:05:43 +0300

    Summarizer v2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,16 +7,31 @@ public class Main {
         System.out.println("Numbers summarizer v1.0.");
         System.out.println("Enter two numbers:");
 
-        int number1;
-        int number2;
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("Number 1:");
-        number1 = scanner.nextInt();
+        int number1 = scanner.nextInt();
+
+        System.out.println("Which operation is it: '+' or '-' ?");
+        String operation = scanner.next();
 
         System.out.println("Number 2:");
-        number2 = scanner.nextInt();
+        int number2 = scanner.nextInt();
 
-        System.out.println("Result:" + (number1 + number2));
+        int result;
+        if (operation.equals("-")) {
+            result = minus(number1, number2);
+        } else {
+            result = plus(number1, number2);
+        }
+
+        System.out.println("Result:" + result);
+    }
+
+    public static int plus(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static int minus(int num1, int num2) {
+        return num1 - num2;
     }
 }
