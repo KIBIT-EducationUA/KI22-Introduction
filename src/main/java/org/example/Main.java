188d58764cbe69e46de7cdc9372a922931c326ab
Your Name <you@example.com>
2023-10-03 22:50:58 +0300

    Summarizer v1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Numbers summarizer v1.0.");
+        System.out.println("Enter two numbers:");
+
+        int number1;
+        int number2;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Number 1:");
+        number1 = scanner.nextInt();
+
+        System.out.println("Number 2:");
+        number2 = scanner.nextInt();
+
+        System.out.println("Result:" + (number1 + number2));
     }
 }
