59b52132838fa4f85616d3f0100ae749f0ba46c7
Your Name <you@example.com>
2023-10-03 23:09:29 +0300

    Summarizer v3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,27 +4,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Numbers summarizer v1.0.");
-        System.out.println("Enter two numbers:");
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Number 1:");
-        int number1 = scanner.nextInt();
 
-        System.out.println("Which operation is it: '+' or '-' ?");
+        System.out.println("Numbers summarizer v1.0.");
+        System.out.println("Summarizer ('+') or subtractor('-') ?");
         String operation = scanner.next();
 
-        System.out.println("Number 2:");
-        int number2 = scanner.nextInt();
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Number 1:");
+            int number1 = scanner.nextInt();
 
-        int result;
-        if (operation.equals("-")) {
-            result = minus(number1, number2);
-        } else {
-            result = plus(number1, number2);
+            System.out.println("Number 2:");
+            int number2 = scanner.nextInt();
+
+            int result;
+            if (operation.equals("-")) {
+                result = minus(number1, number2);
+            } else {
+                result = plus(number1, number2);
+            }
+
+            System.out.println("Result: " + result + "\n");
         }
-
-        System.out.println("Result:" + result);
     }
 
     public static int plus(int num1, int num2) {
