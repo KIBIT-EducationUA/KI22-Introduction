271eba409c7dbf432268fb54bfcadd5114b84f8d
Your Name <you@example.com>
2023-10-04 12:31:46 +0300

    lesson 5 result

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,40 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int number1;
+        int number2;
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number 1:");
+        number1 = scanner.nextInt();
+
+        System.out.println("Enter operation:");
+        String operation = scanner.next();
+
+        System.out.println("Enter number 2:");
+        number2 = scanner.nextInt();
+
+        int result;
+        if (operation.equals("-")) {
+            result = minus(number1, number2);
+        } else if (operation.equals("+")) {
+            result = plus(number1, number2);
+        } else {
+            throw new Error("Invalid operation!");
+        }
+
+        System.out.println("Result: " + result);
+    }
+
+    public static int plus(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static int minus(int num1, int num2) {
+        return num1 - num2;
     }
 }
